d752bcda52e2425b89ca7410aa0e5175cea79d91
liuchengsuo <liuchengsuocsharp@163.com>
2020-08-24 18:23:50 +0800

    首页配置OK

--- src/components/home/home.vue
+++ src/components/home/home.vue
@@ -1,17 +1,17 @@
 <template>
 	<div>
-		<Swiper v-bind:autoPlay="true" v-bind:interval="5000">
-			<Slide @click="onImageClick(item)" v-for="(item, index) in adList" :key="index">
+		<Slider v-bind:autoPlay="false" v-bind:interval="5000" height="180px" v-bind:control-btn= "false">
+			<SliderItem @click="onImageClick(item)" v-for="(item, index) in adList" :key="index">
 				<img v-bind:src="item.IMG_URL" />
-			</Slide>
-		</Swiper>
-		<div class="weui-panel__bd">
+			</SliderItem>
+		</Slider>
+		<div @click="onAddressClick" class="weui-panel__bd">
 			<div class="weui-media-box weui-media-box_small-appmsg">
 				<div class="weui-cells">
 					<a class="weui-cell weui-cell_active weui-cell_access weui-cell_example" href="javascript:">
 						<div class="weui-cell__hd"><img src="../../assets/img/location_icon.png" alt="" style="width: 20px; height: 20px; margin-right: 10px; display: block;"></div>
 						<div class="weui-cell__bd weui-cell_primary">
-							<p>{{homeData.address}}</p>
+							<p>{{homeData.location ? homeData.location.address : ""}}</p>
 						</div>
 						<span class="weui-cell__ft"></span>
 					</a>
@@ -20,48 +20,58 @@
 		</div>
 		<div class="weui-panel__hd">品牌介绍</div>
 		<div class="img-list">
-			<img @click="onImageClick(item)" v-for="(item, index) in homeData.imgLsit" :key="index" v-bind:src="item.IMG_URL" />
+			<img @click="onImageClick(item)" v-for="(item, index) in homeData.imgLsit" :key="index" v-bind:src="item" />
 		</div>
 	</div>
 </template>
 
 <script>
-	import {
-		Swiper,
-		Slide
-	} from 'vue-swiper-component'
+	
+	import { Slider, SliderItem } from 'vue-easy-slider'
 
 	export default {
 		components: {
-			Swiper,
-			Slide
+			Slider,
+			SliderItem
 		},
 		data: function() {
 			return {
-				adList: [{
-						IMG_URL: "https://sslcdn.uboche.com/20191225/5e031504cd832.jpg"
-					},
-					{
-						IMG_URL: "https://sslcdn.uboche.com/20191225/5e031504cd832.jpg"
-					}
-				],
-				homeData: {
-					address: "公司地址",
-					imgLsit: [{
-						IMG_URL: "https://sslcdn.uboche.com/20191225/5e031504cd832.jpg"
-					}, {
-						IMG_URL: "https://sslcdn.uboche.com/20191225/5e031504cd832.jpg"
-					}, {
-						IMG_URL: "https://sslcdn.uboche.com/20191225/5e031504cd832.jpg"
-					}, {
-						IMG_URL: "https://sslcdn.uboche.com/20191225/5e031504cd832.jpg"
-					}]
-				}
+				adList: [],
+				homeData: {}
 			}
+		},
+		mounted: function(){
+			var $this = this
+			this.$get({
+				loadingText: "请稍候...",
+				action: "/ad/getAdList",
+				params: null,
+				success: function(result) {
+					$this.adList = result.data
+				},
+				error: function(error) {
+					$this.$toast(error.statusText)
+				}
+			})
+			
+			this.$get({
+				loadingText: "请稍候...",
+				action: "/global/getHomeData",
+				params: null,
+				success: function(result) {
+					$this.homeData = result.data
+				},
+				error: function(error) {
+					$this.$toast(error.statusText)
+				}
+			})
 		},
 		methods: {
 			onImageClick: function(item) {
-				this.$router.push({path: "/gallery", query:{imgUrl: item.IMG_URL} })
+				location.href = item.LINK
+			},
+			onAddressClick: function() {
+				
 			}
 		}
 	}
@@ -70,6 +80,7 @@
 <style scoped>
 	img {
 		width: 100%;
+		height: 100%;
 	}
 
 	.img-list img {
@@ -87,7 +98,8 @@
 		background-color: white;
 		font-size: 20px;
 	}
-	.weui-gallery__opr{
+
+	.weui-gallery__opr {
 		display: none;
 	}
 </style>
---
+++ vue.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+	devServer: {
+		host: 'localhost',
+		port: 8090,
+		proxy: {
+			'/api': {
+				target: 'http://localhost:8080/baonuo_admin_war_exploded/api/',
+				changeOrigin: true,
+				pathRewrite: {
+					'^/api': '/'
+				}
+			}
+		}
+	}
+}
